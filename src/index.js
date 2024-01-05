0a01bfa7eefde29dae0ab4d53bd663071d0548f8
Candy <candy.ongaro@student.moringaschool.com>
2024-01-05 10:33:29 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', (event)=> {
+    event.preventDefault()
+    const input = event.target.searchByID.value
+
+    if (input <= 3 && input > 0) {
+        fetch(`http://localhost:3000/movies/${input}`)
+    .then(res => res.json())
+    .then(data =>{
+        const title = document.getElementById('Title')
+    const summary = document.getElementById('summary')
+    title.innerText = data.title
+    summary.innerText = data.summary
+
+    })
+    
+    }
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
